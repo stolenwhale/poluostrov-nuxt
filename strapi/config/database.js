67d389d64e57283c4c98d6caa3df5a008720f03b
Irina Frolova <44807919+stolenwhale@users.noreply.github.com>
2021-05-05 20:22:52 +0300

    init strapi

---
+++ strapi/config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'poluostrov_strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
+      },
+      options: {},
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0901c88d-e23b-4e62-88ba-47255f0921c3'
+};
