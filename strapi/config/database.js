307164dfe017bda461ce10b6db425dd14b200306
Irina Frolova <44807919+stolenwhale@users.noreply.github.com>
2021-05-05 22:11:21 +0300

    feat(back) - изменена ДБ на mongo, добавлены настройки для docker

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,17 +1,37 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'poluostrov_strapi'),
+//         username: env('DATABASE_USERNAME', 'root'),
+//         password: env('DATABASE_PASSWORD', 'root'),
+//       },
+//       options: {},
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'poluostrov_strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'root'),
+        client: 'mongo',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'poluostrov_mongo'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-      options: {},
+      options: {
+
+      },
     },
   },
 });
